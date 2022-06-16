ea1a4336cb07efbc9722de4e3b0743e524e6b3ba
Milen Savov <msavov92@gmail.com>
2022-06-16 21:51:20 +0300

    init rep

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+		<script src="https://kit.fontawesome.com/8cb05ca837.js" crossorigin="anonymous"></script>
+		<script src="assets/js/owl.carousel.min.js" type="text/javascript"></script>
+		<script src="assets/js/main.js" type="text/javascript"></script>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Revenant</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		
+	<header>
+		<div class="top_nav">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8"></div>
+					<div class="col-md-4"></div>
+				</div>
+			</div>
+		</div>
+
+		<nav>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="logo">
+							
+						</div>
+						<div class="main_nav">
+							<ul>
+								<li>
+									<a href="#">Home</a>
+								</li>
+								<li>
+									<a href="#">Apparel</a>
+								</li>
+								<li>
+									<a href="#">Fashion</a>
+								</li>
+								<li>
+									<a href="#">News</a>
+								</li>
+								<li>
+									<a href="#">Portfolio</a>
+								</li>
+								<li>
+									<a href="#">Contact</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+	</header>
